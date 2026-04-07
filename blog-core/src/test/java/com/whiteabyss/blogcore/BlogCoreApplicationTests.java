2593113eeb4d3fc99380dc68a6081d31f026566b
YYGX-Yeguyi <22102188457@qq.com>
2026-04-07 20:38:48 +0800

    feat: 初始化项目骨架 - SpringBoot后端 + Vue3前端

---
+++ blog-core/src/test/java/com/whiteabyss/blogcore/BlogCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whiteabyss.blogcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
