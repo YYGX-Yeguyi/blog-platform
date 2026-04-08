8166380b36c4635d7e6fd785648a35b084939bac
YYGX-Yeguyi <22102188457@qq.com>
2026-04-08 19:01:52 +0800

    fix: 缺少了@tablename导致扫描失败

--- blog-core/src/main/java/com/whiteabyss/blogcore/entity/Article.java
+++ blog-core/src/main/java/com/whiteabyss/blogcore/entity/Article.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import java.time.LocalDateTime;
 
 @Data
+@TableName("article")
 public class Article {
 
         @TableId(type = IdType.AUTO)
--- blog-core/src/main/java/com/whiteabyss/blogcore/entity/Category.java
+++ blog-core/src/main/java/com/whiteabyss/blogcore/entity/Category.java
@@ -1,9 +1,11 @@
 package com.whiteabyss.blogcore.entity;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import java.time.LocalDateTime;
 
 @Data
+@TableName("category")
 public class Category {
 
     private int id;
