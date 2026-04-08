56d07ffcbb82c8216f6535c0e515ac8709078b67
YYGX-Yeguyi <22102188457@qq.com>
2026-04-08 19:00:42 +0800

    feat: 版本冲突调整mapper的检索方式

--- blog-core/src/main/java/com/whiteabyss/blogcore/BlogCoreApplication.java
+++ blog-core/src/main/java/com/whiteabyss/blogcore/BlogCoreApplication.java
@@ -1,8 +1,10 @@
 package com.whiteabyss.blogcore;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.whiteabyss.blogcore.mapper")
 @SpringBootApplication
 public class BlogCoreApplication {
 
