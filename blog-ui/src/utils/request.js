1e40006cb54c6d3172bcff5ccc7c954224e17721
YYGX-Yeguyi <2210218457@qq.com>
2026-05-19 19:10:08 +0800

    feat: 远端地址和本地地址增加

--- blog-ui/src/utils/request.js
+++ blog-ui/src/utils/request.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-
+// 配置访问是远端地址还是本地地址的端口设置 远端'http://47.116.30.242:8080/api'
 // 注意：加上 :8080 端口
-const API_BASE_URL = 'http://47.116.30.242:8080/api';
-
+const API_BASE_URL = 'http://localhost:8080/api';
 const request = axios.create({
     baseURL: API_BASE_URL,
     timeout: 10000,
